f94e53c3496e51e0cee90916853f773fe627fb13
yasminghassani <yasminghassani@gmail.com>
2018-05-09 11:57:28 +0700

    initial commit

---
+++ stringutil/reverse_test.go
@@ -0,0 +1,12 @@
+package stringutil
+
+import "testing"
+
+func TestReverseToReturnReversedInputString(t *testing.T) {
+	actualResult := Reverse("Hello")
+	var expectedResult = "olleH"
+
+	if actualResult != expectedResult {
+		t.Fatalf("Expected %s but got %s", expectedResult, actualResult)
+	}
+}
